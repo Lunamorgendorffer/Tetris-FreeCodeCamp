f1075ff70ed35abd96289a63164b8e8f8952f0cb
Lunamorgendorffer <nabdou68@gmail.com>
2023-05-27 19:59:53 +0200

    tetrominoes

--- script.js
+++ script.js
@@ -2,3 +2,33 @@ const grid= document.querySelector('.grid')
 const width = 10;
 const height = 20;
 
+// tetrominoes 
+const lTetromino =[
+    [1 , width+1 , width*2+1, 2],
+    [width,width+1,width+2,width*2+2],
+    [1 , width+1 , width*2+1, width*2],
+    [width,width*2,width+2+1,width*2+2]
+    
+];
+
+const zTetromino =[
+    [1 , width+1 , width*2+1, 2],
+    [width,width+1,width+2,width*2+2],
+    [1 , width+1 , width*2+1, width*2],
+    [width,width*2,width+2+1,width*2+2]
+    
+];
+const tTetromino =[
+    [1 , width+1 , width*2+1, 2],
+    [width,width+1,width+2,width*2+2],
+    [1 , width+1 , width*2+1, width*2],
+    [width,width*2,width+2+1,width*2+2]
+    
+];
+const oTetromino =[
+    [1 , width+1 , width*2+1, 2],
+    [width,width+1,width+2,width*2+2],
+    [1 , width+1 , width*2+1, width*2],
+    [width,width*2,width+2+1,width*2+2]
+    
+];
