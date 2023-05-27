7a16f9deec94e0d2370e60d95b6988fdf42a7ca5
Lunamorgendorffer <nabdou68@gmail.com>
2023-05-27 20:10:19 +0200

    update

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const grid= document.querySelector('.grid')
+let squares = array.from(grid.querySelectorAll('div'))
 const width = 10;
 const height = 20;
+let currentPosition = 4;
 
 // tetrominoes 
 const lTetromino =[
@@ -32,3 +34,22 @@ const oTetromino =[
     [width,width*2,width+2+1,width*2+2]
     
 ];
+const iTetromino =[
+    [1 , width+1 , width*2+1, 2],
+    [width,width+1,width+2,width*2+2],
+    [1 , width+1 , width*2+1, width*2],
+    [width,width*2,width+2+1,width*2+2]
+    
+];
+
+
+const theTetrominoes = [ lTetromino,zTetromino,tTetromino,oTetromino,iTetromino,];
+
+//Randomly select Tetromino
+let random = Math.floor(Math.random()*theTetrominoes.length);
+let currentRotation= 0;
+let current = theTetrominoes[random][currentRotation]
+
+// move the tetromino down 
+let currentPosition = 4;
+
